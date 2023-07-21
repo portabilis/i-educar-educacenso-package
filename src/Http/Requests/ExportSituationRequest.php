b4dba8fe43bd984522bb79baa4d2aaae0b69ce18
Edinei Valdameri <edineivaldameri@gmail.com>
2023-07-20 23:16:02 -0300

    Adiciona novas regras de validação na requisição da exportação

--- src/Http/Requests/ExportSituationRequest.php
+++ src/Http/Requests/ExportSituationRequest.php
@@ -2,6 +2,10 @@
 
 namespace iEducar\Packages\Educacenso\Http\Requests;
 
+use App\Models\LegacySchool;
+use iEducar\Packages\Educacenso\Rules\IsNotEmptyInepNumberSchool;
+use iEducar\Packages\Educacenso\Rules\IsNotEmptyInepNumberSchoolClass;
+use iEducar\Packages\Educacenso\Rules\IsNotEmptyInepNumberStudent;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ExportSituationRequest extends FormRequest
@@ -14,6 +18,14 @@ class ExportSituationRequest extends FormRequest
                 'integer',
                 'in:' . implode(',', array_values(config('educacenso.stage-2.years'))),
             ],
+            'school_id' => [
+                'required',
+                'integer',
+                'exists:' . LegacySchool::class . ',cod_escola',
+                new IsNotEmptyInepNumberSchool(),
+                new IsNotEmptyInepNumberSchoolClass(),
+                new IsNotEmptyInepNumberStudent(),
+            ]
         ];
     }
 
@@ -23,6 +35,9 @@ class ExportSituationRequest extends FormRequest
             'year.required' => 'O campo ano é obrigatório.',
             'year.integer' => 'O campo ano deve ser um número inteiro.',
             'year.in' => 'O campo ano deve ser um dos seguintes valores: ' . implode(', ', array_values(config('educacenso.stage-2.years'))) . '.',
+            'school_id.required' => 'O campo escola é obrigatório.',
+            'school_id.integer' => 'O campo escola deve ser um número inteiro.',
+            'school_id.exists' => 'A escola informada não foi localizada.',
         ];
     }
 }
