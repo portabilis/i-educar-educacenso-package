aad5eddbf2016f34125837e853dc4c3f31954ae6
Edinei Valdameri <edineivaldameri@gmail.com>
2023-07-26 12:02:04 -0300

    Adiciona busca dos dados para o Registro 91

--- src/Layout/Export/Situation/Layout2022/SituationRepository.php
+++ src/Layout/Export/Situation/Layout2022/SituationRepository.php
@@ -82,6 +82,42 @@ class SituationRepository extends \iEducar\Packages\Educacenso\Layout\Export\Con
         int $year,
         int $schoolId
     ): array {
-        // TODO: Implement getDataRecord91() method.
+        $enrollments = LegacyEnrollment::query()
+            ->select([
+                'ref_cod_matricula',
+                'ref_cod_turma',
+            ])
+            ->with([
+                'registration:cod_matricula,ref_cod_aluno,ano',
+                'registration.student:cod_aluno',
+                'registration.student.inep:cod_aluno,cod_aluno_inep',
+                'registration.situation:cod_matricula,cod_situacao',
+                'schoolClass:cod_turma,ref_ref_cod_escola',
+                'schoolClass.school:cod_escola',
+                'schoolClass.school.inep:cod_escola,cod_escola_inep',
+                'schoolClass.inep:cod_turma,cod_turma_inep',
+            ])
+            ->whereHas('registration', fn ($query) => $query->where('ano', $year))
+            ->whereHas('schoolClass', fn ($query) => $query->where('ref_ref_cod_escola', $schoolId))
+            ->active()
+            ->get();
+
+        $enrollments = $enrollments->map(function ($enrollment) {
+            return [
+                '1' => 91,
+                '2' => $enrollment->schoolClass->school->inep->number,
+                '3' => $enrollment->schoolClass->getKey(),
+                '4' => $enrollment->schoolClass->inep ? $enrollment->schoolClass->inep->number : null,
+                '5' => $enrollment->registration->student->inep ? $enrollment->registration->student->inep->number : null,
+                '6' => $enrollment->registration->student->getKey(),
+                '7' => null,
+                '8' => null,
+                '9' => null,
+                '10' => null,
+                '11' => convertSituationIEducarToEducacenso($enrollment->registration->situation->cod_situacao),
+            ];
+        });
+
+        return $enrollments->toArray();
     }
 }
