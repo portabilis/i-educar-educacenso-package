d7a2dc7080bc0dca17f7458f2c9798d5115e23f6
Edinei Valdameri <edineivaldameri@gmail.com>
2023-07-26 12:02:19 -0300

    Incrementa regras para o registro 91

--- src/Http/Controllers/ExportSituationController.php
+++ src/Http/Controllers/ExportSituationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use iEducar\Packages\Educacenso\Http\Requests\ExportSituationRequest;
 use iEducar\Packages\Educacenso\Layout\Export\Situation\Layout2022\Record89;
 use iEducar\Packages\Educacenso\Layout\Export\Situation\Layout2022\Record90;
+use iEducar\Packages\Educacenso\Layout\Export\Situation\Layout2022\Record91;
 use iEducar\Packages\Educacenso\Layout\Export\Situation\Layout2022\SituationRepository;
 use Illuminate\Support\Facades\Validator;
 
@@ -17,14 +18,17 @@ class ExportSituationController extends Controller
 
         $array = [
             'escola' => $repository->getDataRecord89($request->get('year'), $request->get('school_id')),
-            'matriculas' => $repository->getDataRecord90($request->get('year'), $request->get('school_id'))
+            'matriculas' => $repository->getDataRecord90($request->get('year'), $request->get('school_id')),
+            'turma_matriculas' => $repository->getDataRecord91($request->get('year'), $request->get('school_id'))
         ];
 
         $rulesRecord89 = new Record89();
         $rulesRecord90 = new Record90($array['matriculas']);
+        $rulesRecord91 = new Record91($array['matriculas']);
 
-        $rules = array_merge($rulesRecord89->rules(), $rulesRecord90->rules());
-        $messages = array_merge($rulesRecord89->messages(), $rulesRecord90->messages());
+
+        $rules = array_merge($rulesRecord89->rules(), $rulesRecord90->rules(), $rulesRecord91->rules());
+        $messages = array_merge($rulesRecord89->messages(), $rulesRecord90->messages(), $rulesRecord91->messages());
 
         $validator = Validator::make($array, $rules, $messages);
 
