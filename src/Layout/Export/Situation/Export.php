e1610660c9a311e9c6c35a13bbe590361348a39c
Edinei Valdameri <edineivaldameri@gmail.com>
2023-07-26 22:00:48 -0300

    Adiciona classe de exportação do arquivo

--- src/Layout/Export/Situation/Export.php
+++ src/Layout/Export/Situation/Export.php
@@ -2,10 +2,37 @@
 
 namespace iEducar\Packages\Educacenso\Layout\Export\Situation;
 
-class Export
-{
-    public function export(int $year, int $schoolId): void
-    {
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 
+class Export implements WithCustomCsvSettings, FromCollection
+{
+    public function __construct(
+        public array $data
+    ) {
+    }
+
+    public function collection()
+    {
+        $collect = collect();
+
+        $collect->push($this->data['escola']);
+
+        $array [] = $this->data['escola'];
+        foreach ($this->data['matriculas'] as $matricula) {
+            $collect->push($matricula);
+        }
+        foreach ($this->data['turma_matriculas'] as $turma_matricula) {
+            $collect->push($turma_matricula);
+        }
+
+        return $collect;
+    }
+
+    public function getCsvSettings(): array
+    {
+        return [
+            'delimiter' => '|',
+        ];
     }
 }
