4b5d3ad0ab60d6cd961794007751d64a3a208e02
Edinei Valdameri <edineivaldameri@gmail.com>
2023-08-11 17:22:20 -0300

    Organiza validação do CPF

--- src/Rules/CPF.php
+++ src/Rules/CPF.php
@@ -18,18 +18,18 @@ class CPF implements ValidationRule
 
         if (strlen($c) != 11 || preg_match("/^{$c[0]}{11}$/", $c)) {
             $fail($this->message);
-        }
+        } else {
+            for ($s = 10, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--) ;
 
-        for ($s = 10, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--) ;
+            if ($c[9] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+                $fail($this->message);
+            }
 
-        if ($c[9] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
-            $fail($this->message);
-        }
+            for ($s = 11, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--) ;
 
-        for ($s = 11, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--) ;
-
-        if ($c[10] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
-            $fail($this->message);
+            if ($c[10] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+                $fail($this->message);
+            }
         }
     }
 }
