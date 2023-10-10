23b7000e43d6df160962714f56870ae6e5d7de5d
Edinei Valdameri <edineivaldameri@gmail.com>
2023-10-10 15:30:17 -0300

    Adiciona arquivos do registro 00 de 2023

---
+++ src/Services/Version2023/Registro00Import.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace iEducar\Packages\Educacenso\Services\Version2023;
+
+use App\Models\City;
+use App\Models\District;
+use App\Models\Educacenso\Registro00;
+use App\Models\Educacenso\RegistroEducacenso;
+use App\Models\LegacySchool;
+use iEducar\Packages\Educacenso\Services\Version2019\Registro00Import as Registro00Import2019;
+use iEducar\Packages\Educacenso\Services\Version2022\Models\Registro00Model;
+
+use function iEducar\Packages\Educacenso\Services\Version2022\transformDBArrayInString;
+
+class Registro00Import extends Registro00Import2019
+{
+    public function import(RegistroEducacenso $model, $year, $user): void
+    {
+        $this->model = $model;
+        $this->year = $year;
+        $this->user = $user;
+        parent::import($model, $year, $user);
+    }
+
+    protected function getOrCreateSchool(): void
+    {
+        parent::getOrCreateSchool();
+
+        $schoolInep = parent::getSchool();
+
+        if (empty($schoolInep)) {
+            return;
+        }
+
+        /** @var LegacySchool $school */
+        $school = $schoolInep->school;
+        $model = $this->model;
+
+        if (! $school->iddis) {
+            $ibge_code = explode($model->codigoIbgeMunicipio, $model->codigoIbgeDistrito)[1];
+            $city = City::where('ibge_code', $model->codigoIbgeMunicipio)->first();
+
+            $district = District::where('city_id', $city->id)
+                ->where('ibge_code', $ibge_code)
+                ->first();
+
+            $school->iddis = $district->getKey();
+        }
+
+        $school->save();
+    }
+
+    /**
+     * @return Registro00|RegistroEducacenso
+     */
+    public static function getModel($arrayColumns)
+    {
+        $registro = new Registro00Model();
+        $registro->hydrateModel($arrayColumns);
+
+        return $registro;
+    }
+}
