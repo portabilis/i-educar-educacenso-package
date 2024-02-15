a5a8194416eb1450df94c532afe9c133b8d134d7
Edinei Valdameri <edineivaldameri@gmail.com>
2024-02-15 16:36:20 -0300

    Refatora para reaproveitar a mesma busca de dados

--- src/Layout/Export/Situation/Layout2023/SituationRepository.php
+++ src/Layout/Export/Situation/Layout2023/SituationRepository.php
@@ -44,46 +44,7 @@ class SituationRepository extends \iEducar\Packages\Educacenso\Layout\Export\Con
         int $year,
         int $schoolId
     ): array {
-        $dataBaseEducacenso = config('educacenso.data_base.' . $year);
-
-        $enrollments = LegacyEnrollment::query()
-            ->select([
-                'ref_cod_matricula',
-                'ref_cod_turma',
-                'data_enturmacao'
-            ])
-            ->with([
-                'registration:cod_matricula,ref_cod_aluno,ano',
-                'registration.student:cod_aluno',
-                'registration.student.inep:cod_aluno,cod_aluno_inep',
-                'registration.situation:cod_matricula,cod_situacao',
-                'schoolClass' => function ($q): void {
-                    $q->select([
-                        'cod_turma',
-                        'ref_ref_cod_escola',
-                        'tipo_atendimento',
-                        'etapa_educacenso'
-                    ]);
-                    $q->where('tipo_atendimento', TipoAtendimentoTurma::ESCOLARIZACAO);
-                },
-                'schoolClass.school:cod_escola',
-                'schoolClass.school.inep:cod_escola,cod_escola_inep',
-                'schoolClass.inep:cod_turma,cod_turma_inep',
-            ])
-            ->where('data_enturmacao', '<=', $dataBaseEducacenso)
-            ->whereHas('registration', function ($q) use ($year, $dataBaseEducacenso) {
-                $q->where('ano', $year);
-                $q->where(function ($q) use ($dataBaseEducacenso) {
-                    $q->whereNull('data_cancel');
-                    $q->orWhere('data_cancel', '>=', $dataBaseEducacenso);
-                });
-            })
-            ->whereHas('schoolClass', function ($q) use ($schoolId): void {
-                $q->where('ref_ref_cod_escola', $schoolId);
-                $q->where('tipo_atendimento', TipoAtendimentoTurma::ESCOLARIZACAO);
-            })
-            ->whereValid()
-            ->get();
+        $enrollments = $this->getEnrollmentsToExport($year, $schoolId);
 
         $enrollments = $enrollments->map(function ($enrollment) {
             return [
@@ -105,9 +66,32 @@ class SituationRepository extends \iEducar\Packages\Educacenso\Layout\Export\Con
         int $year,
         int $schoolId
     ): array {
+        $enrollments = $this->getEnrollmentsToExport($year, $schoolId);
+
+        $enrollments = $enrollments->map(function ($enrollment) {
+            return [
+                '1' => 91,
+                '2' => $enrollment->schoolClass->school->inep->number,
+                '3' => $enrollment->schoolClass->getKey(),
+                '4' => $enrollment->schoolClass->inep ? $enrollment->schoolClass->inep->number : null,
+                '5' => $enrollment->registration->student->inep ? $enrollment->registration->student->inep->number : null,
+                '6' => $enrollment->registration->student->getKey(),
+                '7' => null,
+                '8' => null,
+                '9' => null,
+                '10' => null,
+                '11' => convertSituationIEducarToEducacenso($enrollment->registration->situation->cod_situacao, $enrollment->schoolClass->etapa_educacenso),
+            ];
+        });
+
+        return $enrollments->toArray();
+    }
+
+    private function getEnrollmentsToExport($year, $schoolId)
+    {
         $dataBaseEducacenso = config('educacenso.data_base.' . $year);
 
-        $enrollments = LegacyEnrollment::query()
+        return LegacyEnrollment::query()
             ->select([
                 'ref_cod_matricula',
                 'ref_cod_turma',
@@ -145,23 +129,5 @@ class SituationRepository extends \iEducar\Packages\Educacenso\Layout\Export\Con
             })
             ->whereValid()
             ->get();
-
-        $enrollments = $enrollments->map(function ($enrollment) {
-            return [
-                '1' => 91,
-                '2' => $enrollment->schoolClass->school->inep->number,
-                '3' => $enrollment->schoolClass->getKey(),
-                '4' => $enrollment->schoolClass->inep ? $enrollment->schoolClass->inep->number : null,
-                '5' => $enrollment->registration->student->inep ? $enrollment->registration->student->inep->number : null,
-                '6' => $enrollment->registration->student->getKey(),
-                '7' => null,
-                '8' => null,
-                '9' => null,
-                '10' => null,
-                '11' => convertSituationIEducarToEducacenso($enrollment->registration->situation->cod_situacao, $enrollment->schoolClass->etapa_educacenso),
-            ];
-        });
-
-        return $enrollments->toArray();
     }
 }
