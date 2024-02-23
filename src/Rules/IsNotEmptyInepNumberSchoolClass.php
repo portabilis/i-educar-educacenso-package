8e44793a6a8729b9af9104dc22f48725eb88a82a
Edinei Valdameri <edineivaldameri@gmail.com>
2024-02-23 09:49:30 -0300

    Valida turmas diretamente para evitar replicação de erros

--- src/Rules/IsNotEmptyInepNumberSchoolClass.php
+++ src/Rules/IsNotEmptyInepNumberSchoolClass.php
@@ -2,7 +2,9 @@
 
 namespace iEducar\Packages\Educacenso\Rules;
 
+use App\Models\LegacySchoolClass;
 use Closure;
+use iEducar\Modules\Educacenso\Model\TipoAtendimentoTurma;
 use iEducar\Packages\Educacenso\Helpers\ErrorMessage;
 use Illuminate\Contracts\Validation\DataAwareRule;
 use Illuminate\Contracts\Validation\ValidationRule;
@@ -16,42 +18,45 @@ class IsNotEmptyInepNumberSchoolClass implements ValidationRule, DataAwareRule
         $year = $this->data['year'];
         $shool_id = $this->data['school_id'];
 
-        $classRepository = 'iEducar\Packages\Educacenso\Layout\Export\Situation\Layout' . $this->data['year'] . '\SituationRepository';
-        $repository = new $classRepository();
-        $enrollments = $repository->getEnrollments90ToExport($year, $shool_id);
+        $schoolClasses = LegacySchoolClass::query()
+            ->active()
+            ->whereSchool($shool_id)
+            ->whereYearEq($year)
+            ->where('tipo_atendimento', TipoAtendimentoTurma::ESCOLARIZACAO)
+            ->get();
 
-        foreach ($enrollments as $enrollment) {
+        foreach ($schoolClasses as $schoolClass) {
             $errorMessage = new ErrorMessage($fail, [
                 'key' => 'cod_turma',
                 'breadcrumb' => 'Escolas -> Cadastros -> Turmas -> Dados Adicionais -> Código INEP',
-                'value' => $enrollment->registration->schoolClass->getKey(),
-                'url' => 'intranet/educar_turma_cad.php?cod_turma=' . $enrollment->registration->schoolClass->getKey()
+                'value' => $schoolClass->getKey(),
+                'url' => 'intranet/educar_turma_cad.php?cod_turma=' . $schoolClass->getKey()
             ]);
 
-            if (is_null($enrollment->registration->schoolClass->inep)) {
+            if (is_null($schoolClass->inep)) {
                 $errorMessage->toString([
-                    'message' => 'Dados para formular o registro 90 inválidos. A turma ' . $enrollment->registration->schoolClass->nm_turma . ' não possui um número INEP.',
+                    'message' => 'Dados para formular o registro 90 inválidos. A turma ' . $schoolClass->nm_turma . ' não possui um número INEP.',
                 ]);
                 continue;
             }
 
-            if (is_null($enrollment->registration->schoolClass->inep->cod_turma_inep)) {
+            if (is_null($schoolClass->inep->cod_turma_inep)) {
                 $errorMessage->toString([
-                    'message' => 'Dados para formular o registro 90 inválidos. A turma ' . $enrollment->registration->schoolClass->nm_turma . ' não possui um número INEP.',
+                    'message' => 'Dados para formular o registro 90 inválidos. A turma ' . $schoolClass->nm_turma . ' não possui um número INEP.',
                 ]);
                 continue;
             }
 
-            if (strlen($enrollment->registration->schoolClass->inep->cod_turma_inep) > 10) {
+            if (strlen($schoolClass->inep->cod_turma_inep) > 10) {
                 $errorMessage->toString([
-                    'message' => 'Dados para formular o registro 90 inválidos. A turma ' . $enrollment->registration->schoolClass->nm_turma . ' possui um número INEP com mais de 10 digitos.',
+                    'message' => 'Dados para formular o registro 90 inválidos. A turma ' . $schoolClass->nm_turma . ' possui um número INEP com mais de 10 digitos.',
                 ]);
                 continue;
             }
 
-            if (! is_numeric($enrollment->registration->schoolClass->inep->cod_turma_inep)) {
+            if (! is_numeric($schoolClass->inep->cod_turma_inep)) {
                 $errorMessage->toString([
-                    'message' => 'Dados para formular o registro 90 inválidos. A Turma ' . $enrollment->registration->schoolClass->nm_turma . ' não possui um número INEP válido.',
+                    'message' => 'Dados para formular o registro 90 inválidos. A Turma ' . $schoolClass->nm_turma . ' não possui um número INEP válido.',
                 ]);
             }
         }
