e474db8b820a3293cea0aec4d0a091c0e8552b43
Edinei Valdameri <edineivaldameri@gmail.com>
2025-06-10 09:15:55 -0300

    Ajusta para novo tipo de campo

--- src/Rules/IsNotEmptyInepNumberSchoolClass.php
+++ src/Rules/IsNotEmptyInepNumberSchoolClass.php
@@ -23,7 +23,7 @@ class IsNotEmptyInepNumberSchoolClass implements ValidationRule, DataAwareRule
             ->whereSchool($shool_id)
             ->whereYearEq($year)
             ->where('nao_informar_educacenso', '!=', 1)
-            ->where('tipo_atendimento', TipoAtendimentoTurma::CURRICULAR_ETAPA_ENSINO)
+            ->whereRaw(TipoAtendimentoTurma::CURRICULAR_ETAPA_ENSINO . ' = ANY(tipo_atendimento)')
             ->get();
 
         foreach ($schoolClasses as $schoolClass) {
